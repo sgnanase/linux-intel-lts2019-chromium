0f9b2778bb133df63d4b4626db0168300b938ac1
Eric Biggers <ebiggers@google.com>
2020-09-16 21:11:24 -0700

    fscrypt: add fscrypt_prepare_new_inode() and fscrypt_set_context()
    fscrypt_get_encryption_info() is intended to be GFP_NOFS-safe.  But
    actually it isn't, since it uses functions like crypto_alloc_skcipher()
    which aren't GFP_NOFS-safe, even when called under memalloc_nofs_save().
    Therefore it can deadlock when called from a context that needs
    GFP_NOFS, e.g. during an ext4 transaction or between f2fs_lock_op() and
    f2fs_unlock_op().  This happens when creating a new encrypted file.
    
    We can't fix this by just not setting up the key for new inodes right
    away, since new symlinks need their key to encrypt the symlink target.
    
    So we need to set up the new inode's key before starting the
    transaction.  But just calling fscrypt_get_encryption_info() earlier
    doesn't work, since it assumes the encryption context is already set,
    and the encryption context can't be set until the transaction.
    
    The recently proposed fscrypt support for the ceph filesystem
    (https://lkml.kernel.org/linux-fscrypt/20200821182813.52570-1-jlayton@kernel.org/T/#u)
    will have this same ordering problem too, since ceph will need to
    encrypt new symlinks before setting their encryption context.
    
    Finally, f2fs can deadlock when the filesystem is mounted with
    '-o test_dummy_encryption' and a new file is created in an existing
    unencrypted directory.  Similarly, this is caused by holding too many
    locks when calling fscrypt_get_encryption_info().
    
    To solve all these problems, add new helper functions:
    
    - fscrypt_prepare_new_inode() sets up a new inode's encryption key
      (fscrypt_info), using the parent directory's encryption policy and a
      new random nonce.  It neither reads nor writes the encryption context.
    
    - fscrypt_set_context() persists the encryption context of a new inode,
      using the information from the fscrypt_info already in memory.  This
      replaces fscrypt_inherit_context().
    
    Temporarily keep fscrypt_inherit_context() around until all filesystems
    have been converted to use fscrypt_set_context().
    
    Acked-by: Jeff Layton <jlayton@kernel.org>
    Link: https://lore.kernel.org/r/20200917041136.178600-2-ebiggers@kernel.org
    Signed-off-by: Eric Biggers <ebiggers@google.com>
    (cherry picked from commit a4d695eb739715aeca32c5f6f8e3dd4689012779)

--- include/linux/fscrypt.h
+++ include/linux/fscrypt.h
@@ -158,6 +158,7 @@ int fscrypt_ioctl_get_nonce(struct file *filp, void __user *arg);
 int fscrypt_has_permitted_context(struct inode *parent, struct inode *child);
 int fscrypt_inherit_context(struct inode *parent, struct inode *child,
 			    void *fs_data, bool preload);
+int fscrypt_set_context(struct inode *inode, void *fs_data);
 
 struct fscrypt_dummy_context {
 	const union fscrypt_context *ctx;
@@ -186,6 +187,8 @@ extern int fscrypt_unregister_key_removal_notifier(struct notifier_block *nb);
 
 /* keysetup.c */
 int fscrypt_get_encryption_info(struct inode *inode);
+int fscrypt_prepare_new_inode(struct inode *dir, struct inode *inode,
+			      bool *encrypt_ret);
 void fscrypt_put_encryption_info(struct inode *inode);
 void fscrypt_free_inode(struct inode *inode);
 int fscrypt_drop_inode(struct inode *inode);
@@ -349,6 +352,11 @@ static inline int fscrypt_inherit_context(struct inode *parent,
 	return -EOPNOTSUPP;
 }
 
+static inline int fscrypt_set_context(struct inode *inode, void *fs_data)
+{
+	return -EOPNOTSUPP;
+}
+
 struct fscrypt_dummy_context {
 };
 
@@ -406,6 +414,15 @@ static inline int fscrypt_unregister_key_removal_notifier(
 static inline int fscrypt_get_encryption_info(struct inode *inode)
 {
 	return -EOPNOTSUPP;
+}
+
+static inline int fscrypt_prepare_new_inode(struct inode *dir,
+					    struct inode *inode,
+					    bool *encrypt_ret)
+{
+	if (IS_ENCRYPTED(dir))
+		return -EOPNOTSUPP;
+	return 0;
 }
 
 static inline void fscrypt_put_encryption_info(struct inode *inode)
